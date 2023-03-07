3e99956e97d0324b370a2b089c0d77da7950b4e0
Kallesh K C <sapfiori4gitios@gmail.com>
2023-03-07 23:26:15 +0530

    third heading

--- App.js
+++ App.js
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom/client";
 
 const heading = React.createElement("h1",{id: "heading",},"Heading1");
 const heading2 = React.createElement("h2",{id: "heading2",},"Heading2");
-const container = React.createElement("div",{id: "container",},[heading,heading2]);
+const heading3 = React.createElement("h2",{id: "heading2",},"Heading3");
+const container = React.createElement("div",{id: "container",},[heading,heading2,heading3]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(container);
